0b598d4e2165ab7dbe29309faeb0948d59aabd20
Wardi <wardi@nusa.net.id>
2026-03-20 22:19:23 +0700

    feat: refine lookup query to exclude specific service groups (SLBP, MT, IP)

--- index.ts
+++ index.ts
@@ -73,20 +73,23 @@ app.post(
       const queryResults = await Promise.all(
         batches.map(
           (batch) => sql`
-        SELECT 
-            cst.Network AS network, 
-            (ss.NormalDownCeil * 1000) AS download_rate, 
-            (ss.NormalUpCeil * 1000) AS upload_rate, 
-            'bps' AS unit, 
-            cs.ServiceId AS subscription_package 
-        FROM 
-            CustomerServiceTechnical AS cst 
-        LEFT JOIN 
-            CustomerServices AS cs ON cs.CustServId = cst.CustServId 
-        LEFT JOIN 
-            ServiceShaping AS ss ON cs.ServiceId = ss.ServiceId 
-        WHERE 
-            cst.Network IN ${sql(batch)}
+        SELECT
+            cst.Network AS network,
+            (ss.NormalDownCeil * 1000) AS download_rate,
+            (ss.NormalUpCeil * 1000) AS upload_rate,
+            'bps' AS unit,
+            cs.ServiceId AS subscription_package
+        FROM
+            CustomerServiceTechnical AS cst
+        LEFT JOIN
+            CustomerServices AS cs ON cs.CustServId = cst.CustServId
+        LEFT JOIN
+            Services AS s ON cs.ServiceId = s.ServiceId
+        LEFT JOIN
+            ServiceShaping AS ss ON cs.ServiceId = ss.ServiceId
+        WHERE
+            NOT (s.ServiceGroup IN ('SLBP', 'MT', 'IP'))
+            AND cst.Network IN ${sql(batch)}
       `,
         ),
       )
